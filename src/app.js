58ff0b18615e7c3e9a8e227213b5b25ca932cc03
Yiggum <yiggum123@gmail.com>
2021-11-11 20:52:23 +0000

    prices pushed to array errors too

--- src/app.js
+++ src/app.js
@@ -4,17 +4,21 @@ import axios from 'axios'
 const url = "http://api.coincap.io/v2/assets/amp"
 
 const x = 0
+let currentPrices = []
 
 function getRequest(){
     axios.get(url).then(function (response) {
     // handle success
     console.log("wooho")
-    console.log(response);
+    console.log(response.data.data)
+    currentPrices.push(response.data.data.priceUsd)
+    console.log(currentPrices);
     })
     .catch(function (error) {
     // handle error
     console.log("ohshit")
-    console.log(error);
+    console.log(error)
+    currentPrices.push("error");
     })
 }
 
