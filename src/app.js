f2bccc652d54264f805f5086182b1e7370baa977
Charles Wilson <charlesw124@gmail.com>
2021-11-13 16:30:09 +0000

    add PriceEntity class

--- src/app.js
+++ src/app.js
@@ -11,10 +11,11 @@ console.log(token)
 const url = "http://api.coincap.io/v2/assets/amp"
 
 const x = 0
-let y = 0
+let mainLoopTimer = 0
 let currentPrices = []
 let currentZero = 0
 let current59 = 59
+let arrayOfPriceObjects = []
 
 function getRequest() {
     axios.get(url,{
@@ -35,14 +36,15 @@ function getRequest() {
             currentPrices.push("error");
         })
 }
-
+// main loop for pricechecker
 do {
     let myVar = setInterval(function () { timer() }, 1000);
 
     function timer() {
         getRequest()
         averageCheckerWithDefence()
-        y++
+        console.log(arrayOfPriceObjects)
+        mainLoopTimer++
 
     }
     function stopFunction() {
@@ -52,27 +54,31 @@ do {
 } while (x > 0);
 
 
-
-function averageChecker(x, y) {
+// console logs rolling average over the past min
+function rollingAverageChecker(x, y) {
     // currentPrices[0]
     // currentPrices[59]
     var percDiff = 100 * Math.abs((x - y) / ((x + y) / 2));
     console.log(percDiff)
     if (currentPrices[currentZero] < currentPrices[current59]) {
         console.log("Increase")
+        
+        arrayOfPriceObjects.push(new PriceEntity(percDiff, "increase"))
     } else {
         console.log("decrease")
+        
+        arrayOfPriceObjects.push(new PriceEntity(percDiff, "decrease"))
     }
 }
-
+// if errors check 
 function averageCheckerWithDefence() {
 
-    if (y > 60) {
+    if (mainLoopTimer > 60) {
         if (currentPrices[currentZero] === "error" || currentPrices[current59] === "error") {
             currentZero++
             current59++
         } else {
-            averageChecker(currentPrices[currentZero], currentPrices[current59])
+            rollingAverageChecker(currentPrices[currentZero], currentPrices[current59])
             currentZero++
             current59++
 
@@ -80,5 +86,27 @@ function averageCheckerWithDefence() {
     } else {
         console.log("It is not time")
     }
+}
+
+/*
+TODO make a rolling min average function 
+Where after the first min it totals up currentPrices[0]-[60]
+states whether increase or decrease
+*/
+
+// function minuetAverage(){
+//     if (Number.isInteger(mainLoopTimer / 60)){
+//         for(i = 0; i< currentPrices.length; i++){
+//             currentPrices[i]
+//         }
+//     }
+// }
+
+
+class PriceEntity {
+    constructor(price, upOrDown) {
+        this.percentageDifference = price;
+        this.upOrDown = upOrDown;
+      }
 }
 
